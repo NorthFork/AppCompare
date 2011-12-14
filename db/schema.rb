36dcb2cde6b4f0f1a7aebe60bf53e6cc38c40d7a
Brian Feifarek <bfeifarek@q.com>
2011-12-14 12:49:16 -0700

    Add schema after running rake db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,102 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20111214060319) do
+
+  create_table "app_features", :force => true do |t|
+    t.integer  "app_id"
+    t.integer  "feature_id"
+    t.integer  "feature_status_id"
+    t.integer  "source_id"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "app_products", :force => true do |t|
+    t.integer  "app_id"
+    t.integer  "product_id"
+    t.integer  "min_appversion_id"
+    t.integer  "max_appversion_id"
+    t.text     "whoisusing"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "app_versions", :force => true do |t|
+    t.integer  "app_id"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "apps", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "feature_categories", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "feature_statuses", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "features", :force => true do |t|
+    t.integer  "feature_category_id"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", :force => true do |t|
+    t.integer  "vendor_id"
+    t.string   "name"
+    t.string   "description"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sources", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "vendors", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "url"
+    t.string   "phone"
+    t.string   "contact"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
